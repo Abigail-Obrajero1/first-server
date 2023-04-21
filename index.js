93ad23662cf10b7985afc426852a1bac9a505eca
Abigail Obrajero <abigailobrajero@gmail.com>
2023-04-21 16:05:00 +0000

    ⭐Creando servidor

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("hello world!!!");
+// 1. importando el modulo http
+
+import http from 'http';
+
+
+// 2. crear el servidor
+
+const server = http.createServer((req, res) => {
+    // logica del server
+    // 1. respondiendo al cliente
+    res.write('Hello from the server...');
+    // 2. cerrar la conexion
+    res.end();
+});
+
+// 3. arrancar el servidor
+server.listen(3000,"0.0.0.0",()=>{
+    console.log("Servidor escuchando en http://localhost:3000");
+});
