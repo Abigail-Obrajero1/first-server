8cf81a45f80189d2f381592c7540383b39f1c72b
Abigail Obrajero <abigailobrajero@gmail.com>
2023-04-25 15:12:27 +0000

    ⭐Creando logger de peticiones

--- index.js
+++ index.js
@@ -2,19 +2,32 @@
 import http from 'http';
 // Biblioteca path
 import path from 'path';
+import { runInNewContext } from 'vm';
 // Recreando Built-in variables
 global["__dirname"] = path.dirname(new URL(import.meta.url).pathname);
 global["__filename"] = path.join(new URL(import.meta.url).pathname);
 
 // 2. crear el servidor
 
-const server = http.createServer(( _, res) => {
-    // logica del server
+const server = http.createServer(( req, res) => {
+    console.log(`📣 CLIENT-REQUEST:${req.method} ${req.url}`);
+
+    res.setHeader('Content-Type','text/html')
+    res.write("<head>");
+    res.write("<head>");
+    res.write("<title>My app");
+    res.write("</title>");
+    res.write("</head>");
+    res.write("<body>");
+    res.write("<h1>Hello from my server...</h1>")
+    res.write("</body>");
+    res.write("</html>");
+    // Creando un logger de peticiones
+    // toda logica del server
     // 1. respondiendo al cliente
-    res.write(`
-    __dirname:${__dirname}
-    __filename:${__filename}`);
+   console.log(`Se ha contestado recurso: ${req.method} ${req.url}`);
     // 2. cerrar la conexion
+    console.log("📣 Cerrando la conexion");
     res.end();
 });
 
